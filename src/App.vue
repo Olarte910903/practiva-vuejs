09a9a1d737b12ed950343cd604227ec2b59da145
Miguel Olarte <mbsic91@gmail.com>
2023-05-18 21:47:23 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script>
+import StartButton from './components/StartButton.vue';
+export default{
+
+  data(){
+    return {
+      titulo: "Práctica Vue JS",
+      elementos: ["Calcular", "Añadir", "Eliminar", "Borrar"],
+      cantidad: 0
+    }
+  },
+  components: {
+    StartButton
+  },
+  methods: {
+    sumar(){
+      this.cantidad = this.cantidad + 1;
+    },
+    restar(){
+      this.cantidad = this.cantidad - 1;
+    },
+    multiplicar(){
+      this.cantidad = this.cantidad * 2;
+    },
+    borrar(){
+      this.cantidad = 0;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="contenedor">
+  <h1>{{ titulo }}</h1>
+  <p>{{ cantidad }}</p>
+  <div class="contenedor-botones">
+
+    <StartButton :nombreVariable="elementos[2]" @click = "restar"/>
+    <StartButton :nombreVariable="elementos[1]" @click = "sumar"/>
+    <StartButton :nombreVariable="elementos[0]" @click = "multiplicar"/>
+    <StartButton :nombreVariable="elementos[3]" @click = "borrar"/>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+.contenedor{
+  $default-font: 'Oswald';
+  h1{
+    font-family:$default-font;
+    color: green;
+    }
+    p{
+      font-family: $default-font;
+      font-size: 30px;
+    }
+  .contenedor-botones{
+    display:flex;
+    flex-direction: column;
+    gap:20px;
+  }
+  }
+</style>
---
+++ src/components/StartButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button>
+    {{ nombreVariable }}
+  </button>
+</template>
+
+<script>
+export default{
+  props: {
+    nombreVariable: {
+      default: "Botón"
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+ $default-font: 'Oswald';
+  button{
+    width: 120px;
+    height: 50px;
+    border-radius: 10px;
+    background: purple;
+    color:white;
+    font-family: $default-font;
+    font-size: 18px;
+    border: none;
+    &:hover{
+      background: violet;
+      color:black;
+    }
+  }
+</style>
